0f4c7154941c4506b025a33ffa92fe00e7f38c66
giovannipc <giovannipaulocunha@hotmail.com>
2019-07-15 13:29:36 -0300

    commit teste

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const ensureLogin = require('connect-ensure-login');
 
 // User model
 const User = require("../models/user");
+const Events = require('../models/events');
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
@@ -86,13 +87,45 @@ authRoutes.get('/home', ensureLogin.ensureLoggedIn('/login'), (req, res) => {
 });
 
 authRoutes.get('/events', ensureLogin.ensureLoggedIn('/login'), (req, res) => {
+
   Events.find()
-  .then(allEvents => res.render('auth/allevents', allEvents))
+  .then(allEvents => res.render('auth/allevents', { allEvents }))
   .catch(err => console.log(err))
+
 });
 
 authRoutes.get('/createvents', ensureLogin.ensureLoggedIn('/login'), (req, res) => {
   res.render('auth/createvents');
+});
+
+authRoutes.post('/createvents', ensureLogin.ensureLoggedIn('/login'), (req, res) => {
+  const { title, type, description } = req.body;
+  const newEvent = new Events ({
+    title: title,
+    type: type,
+    description: description,
+    owner : req.user.id
+  })
+
+  newEvent.save().then(event => {
+    console.log('Success', event)
+  })
+  .catch(error => {
+    console.log('Error', eror)
+  })
+
+  res.render('auth/events');
+});
+
+authRoutes.get('/event/:ID', ensureLogin.ensureLoggedIn('/login'), (req, res) => {
+
+  const eventID = req.params.ID;
+  Events.findById(eventID)
+  .then(event => {
+    res.render('auth/event', event);
+  })
+  .catch(err => console.log(err))
+  
 });
 
 module.exports = authRoutes;
